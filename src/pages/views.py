d281812053678ccc463ab1a2e4fb9f918963b2a8
Testo-Toaster <schidlowskiantonio@gmail.com>
2026-04-09 11:16:40 +0000

    Einfügen von der ersten View

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# src/pages/views.py
+from django.http import HttpResponse
+
+def home_page_view(request):
+    return HttpResponse("Hello, World od Django!")
